a19cee7653d29d0f4826f4d7e76c8415d580ab33
Seo-JeongJin <tj0wjd9wls8@gmail.com>
2026-01-22 21:18:18 +0900

    map list rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import Notification from './components/Notification';
+import { useState } from 'react';
 
 export default function App() {
+  const [items, setItems] = useState<string[]>(['사과', '바나나', '오렌지']);
+  const addFruit = () => {
+    setItems((items) => [...items, '포도']);
+  };
   return (
     <>
-      <Notification />
+      <h1>Fruits List</h1>
+      <ul>
+        {items.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+      <button onClick={addFruit} disabled={items.length >= 4}>
+        과일 추가
+      </button>
     </>
   );
 }
