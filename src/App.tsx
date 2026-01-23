455c5e558b73616c4fe77971bdb446ff47b4790d
Seo-JeongJin <tj0wjd9wls8@gmail.com>
2026-01-23 11:31:37 +0900

    recipe list redering test, set tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import { useState } from 'react';
+import RecipeList from './components/RecipeList';
 
 export default function App() {
-  const [items, setItems] = useState<string[]>(['사과', '바나나', '오렌지']);
-  const addFruit = () => {
-    setItems((items) => [...items, '포도']);
-  };
   return (
     <>
-      <h1>Fruits List</h1>
-      <ul>
-        {items.map((value, index) => (
-          <li key={index}>{value}</li>
-        ))}
-      </ul>
-      <button onClick={addFruit} disabled={items.length >= 4}>
-        과일 추가
-      </button>
+      <RecipeList />
     </>
   );
 }
