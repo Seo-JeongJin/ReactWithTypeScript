466b7320659b97a74ee21fe070bb644faa5f3e50
Seo-JeongJin <tj0wjd9wls8@gmail.com>
2026-01-25 23:58:34 +0900

    controlled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import RecipeList from './components/RecipeList';
+import UserInfoForm from './components/UserInfoForm';
+import './styles/global.css';
+import './styles/index.css';
 
 export default function App() {
   return (
     <>
-      <RecipeList />
+      <UserInfoForm />
     </>
   );
 }
